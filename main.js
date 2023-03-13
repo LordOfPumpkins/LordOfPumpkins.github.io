4df8b3c889864ba1f3183f762ff66c8837b1f0ad
Kende06 <92167417+Kende06@users.noreply.github.com>
2023-03-13 19:26:38 +0100

    Add download logic and <a> element

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
 const optifine = document.getElementById("optifine")
 const skybox = document.getElementById("skybox")
-const download = document.getElementByClass("download")
+const download = document.getElementById("download")
 
 addCheckboxListener(optifine);
 addCheckboxListener(skybox);
 
 function addCheckboxListener (element) {
     element.addEventListener("click", function() {
-        if (optifine.checked && !skybox.checked) {
-            
+        if (!optifine.checked && !skybox.checked) {
+            download.download = "§c§lMORDOR§7-§flight.zip"
         }
-        else {
-    
+        else if (optifine.checked && !skybox.checked) {
+            download.download = "§c§lMORDOR§7-§fof.zip"
+        }
+        else if (optifine.checked && skybox.checked || !optifine.checked && skybox.checked) {
+            download.download = "§c§lMORDOR§7-§fv4.zip"
         }
     })
 }
